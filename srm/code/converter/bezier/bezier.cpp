215cc36975ecd8c818f1634cd5094f58caf0c381
AV3pml30 <aVorotnikov@users.noreply.github.com>
2021-02-27 23:41:04 +0300

    Update bezier sampling
    Accelerate bezier sampling with accuracy with removing if-else construction, fix bug with 2 same points

--- srm/code/converter/bezier/bezier.cpp
+++ srm/code/converter/bezier/bezier.cpp
@@ -67,61 +67,45 @@ std::vector<srm::vec_t> srm::build_bezier_t::Sampling(double accuracy, double st
 
   // choose initial params: delta, t param
   double delta = startDelta, accuracy2 = accuracy * accuracy;
-  double tCur1 = 0, tPrev1 = 0, tCur2 = 1, tPrev2 = 1;
-  std::list<vec_t> resList1, resList2;
+  double tCur[] = {0, 1}, tPrev[] = {0, 1};
+  std::list<vec_t> resList[2];
 
   // add first point
-  vec_t
-    prevPos1 = EvaluatePoint(tPrev1),
-    prevPos2 = EvaluatePoint(tPrev2);
-  resList1.push_back(prevPos1);
-  resList2.push_front(prevPos2);
+  vec_t prevPos[] = {EvaluatePoint(tPrev[0]), EvaluatePoint(tPrev[1])};
+  resList[0].push_back(prevPos[0]);
+  resList[1].push_back(prevPos[1]);
 
-  bool toFirst = 1;
-  while (tCur1 < tCur2) {
-    // case to use first list
-    if (toFirst) {
-      tCur1 = tPrev1 + delta;
-      // calculate new point
-      vec_t curPos = EvaluatePoint(tCur1);
-      // case when point is suitable : add point
-      if ((curPos - prevPos1).Len2() <= accuracy2) {
-        resList1.push_back(curPos);
-        prevPos1 = curPos;
-        tPrev1 = tCur1;
-        toFirst = 0;
-      }
-      // case when point is unsuitable : decrease delta
-      else {
-        delta /= 2;
-        tCur1 = tPrev1;
-      }
+  bool toFirst = 0;
+  if (startDelta >= 1 && (prevPos[0] - prevPos[1]).Len2() > accuracy2)
+    delta = 0.5;
+  while (tCur[0] < tCur[1] - delta) {
+    if (toFirst)
+      tCur[toFirst] = tPrev[toFirst] - delta;
+    else
+      tCur[toFirst] = tPrev[toFirst] + delta;
+    // calculate new point
+    vec_t curPos = EvaluatePoint(tCur[toFirst]);
+    // case when point is suitable : add point
+    if ((curPos - prevPos[toFirst]).Len2() <= accuracy2) {
+      resList[toFirst].push_back(curPos);
+      prevPos[toFirst] = curPos;
+      tPrev[toFirst] = tCur[toFirst];
+      toFirst = !toFirst;
     }
-    // case to use second list
+    // case when point is unsuitable : decrease delta
     else {
-      tCur2 = tPrev2 - delta;
-      // calculate new point
-      vec_t curPos = EvaluatePoint(tCur2);
-      // case when point is suitable : add point
-      if ((curPos - prevPos2).Len2() <= accuracy2) {
-        resList2.push_front(curPos);
-        prevPos2 = curPos;
-        tPrev2 = tCur2;
-        toFirst = 1;
-      }
-      // case when point is unsuitable : decrease delta
-      else {
-        delta /= 2;
-        tCur2 = tPrev2;
-      }
+      delta /= 2;
+      tCur[toFirst] = tPrev[toFirst];
     }
   }
   // create res vector from 2 lists
   std::vector<vec_t> res;
-  res.reserve(resList1.size() + resList2.size());
-  for (auto &point : resList1)
+  res.reserve(resList[0].size() + resList[1].size());
+  for (auto &point : resList[0])
     res.push_back(point);
-  for (auto &point : resList2)
-    res.push_back(point);
+  const auto &listStartIt = resList[1].cbegin();
+  for (auto it = --resList[1].end(); it != listStartIt; it--)
+    res.push_back(*it);
+  res.push_back(*listStartIt);
   return res;
 }
