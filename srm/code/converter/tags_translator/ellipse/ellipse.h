4312698c8fcb7c45b11cafb1eddf7cce5b46ae42
AV3pml30 <39947159+aVorotnikov@users.noreply.github.com>
2021-03-13 21:49:26 +0300

    Add ellipse arc sampling

--- srm/code/converter/tags_translator/ellipse/ellipse.h
+++ srm/code/converter/tags_translator/ellipse/ellipse.h
@@ -28,15 +28,16 @@ namespace srm {
 
   /**
    * Ellipse arc from 'path' tag sampling function
-   * @param[in] x1 first arc point
-   * @param[in] x2 second arc point
+   * @param[in] p1 first arc point
+   * @param[in] p2 second arc point
    * @param[in] radiuses 2 ellipse radiuses by x and y
    * @param[in] fA flag for arc angle size
    * @param[in] fS flag for angle delta
+   * @param[in] phi angle
    * @param[in] accuracy sampling accuracy
    * @return point vector with sampling
    */
-  std::vector<vec_t> EllipseArcSampling(vec_t x1, vec_t x2, vec_t radiuses, bool fA, bool fS, double accuracy);
+  std::vector<vec_t> EllipseArcSampling(vec_t p1, vec_t p2, vec_t radiuses, bool fA, bool fS, double phi, double accuracy);
 }
 
 #endif /* __ELLIPSE_H_INCLUDED */
