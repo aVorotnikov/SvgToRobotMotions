dfcf272d6deaa8581542b873839b8b40c9cf47ef
Ilya Pavlov <iliaauaasd@gmail.com>
2021-03-15 01:59:04 +0300

    Fix transfrom header file doxygen description

--- srm/code/converter/tags_translator/transform/transform.h
+++ srm/code/converter/tags_translator/transform/transform.h
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @brief transform_t class header file
+ * @authors Pavlov Ilya
+ * @date 15.03.2021
+ *
+ * Contains transform_t class description
+ */
+
 #pragma once
 
 #ifndef __TRANSFORM_H_INCLUDED
